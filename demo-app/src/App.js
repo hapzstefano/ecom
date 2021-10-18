52dff2714bfcbfad9f8f257772e49d7ec5e272f7
hapzstefano <stevanocalvin333@gmail.com>
2021-10-18 23:20:17 +0700

    TORONTOTIRTA

---
+++ demo-app/src/App.js
@@ -0,0 +1,30 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import React, { useState } from "react";
+import "./App.css";
+
+//pages
+import NotFound from "./components/Pages/NotFound";
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <h1>saya klebet pinter saya</h1>
+        </Route>
+        <Route exact path="/buku">
+          <h1>saya ga suka baca buku SE</h1>
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ demo-app/src/components/Pages/NotFound/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./notfound.css";
+
+const NotFound = () => {
+  return (
+    <div
+      className="not-found"
+      style={{ backgroundImage: "url('/assets/images/global/drTirta.png')" }}
+    >
+      <div className="not-found-404">
+        <h1
+          style={{
+            backgroundImage: "url('/assets/images/global/not_found.jpg')",
+          }}
+        >
+          Oops!
+        </h1>
+      </div>
+      <h2 style={{ color: "white" }}>404 - Page not found</h2>
+      <p style={{ color: "white" }}>
+        {/* The page you are looking for might have been removed had its name
+        changed or is temporarily unavailable. */}
+        SIANG MENANGANI COVID MALAM BERGERAK DI MID
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
