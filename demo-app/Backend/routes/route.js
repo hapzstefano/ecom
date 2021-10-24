b25383c87d80f03cbca97672681a9edab41b2492
samuel <napogistra@gmail.com>
2021-10-25 01:52:45 +0700

    ini coba

--- demo-app/Backend/routes/route.js
+++ demo-app/Backend/routes/route.js
@@ -5,35 +5,46 @@ const cors = require("cors");
 router.use(cors())
 
 router.post('/register',async (req, res) => {
+    const today = new Date();
+    const date = today.getDate()+(today.getMonth()+1)+today.getFullYear();
     const email = req.body.email;
     const nama = req.body.nama;
     const password = req.body.password;
-    const confirm = req.body.conpass;
+    const confirm = req.body.confirm;
     const address = req.body.address;
-    const checkCustomer = Customer.find({email : email})
-    if (checkCustomer)
-    {
-
-    }
-    if (password == confirm){
-        const Customer = new Customer(
-            {
-                id_cust: 0,
-                nama: nama,
-                poin: 0,
-                email: email,
-                password: password,
-                alamat: address,
-                status: status
+    Customer.count ({id_cust: { $regex: '.*' + date + '.*' }}, function (err,res2){
+        const id_cust = "CU"+date+Number(res2+1)
+        Customer.count({email : email} , function (error,result){
+            console.log(result)
+            if (result == 0) {
+                if (password == confirm) {
+                    const customer = new Customer(
+                        {
+                            id_cust: id_cust,
+                            nama: nama,
+                            poin: 0,
+                            email: email,
+                            password: password,
+                            alamat: address,
+                            status: 0
+                        }
+                    );
+                    customer.save(function (err, book) {
+                        if (err) return console.error(err);
+                        console.log("Created");
+                    });
+                    return res.status(200).send("Berhasil Register");
+                }
             }
-            );
-        Customer.save(function (err, book) {
-            if (err) return console.error(err);
-            console.log("Created");
+            else
+            {
+                return res.status(400).send("Email sudah terdaftar");
+            }
         });
-    }
+    })
 
-return res.status(200).send("created");
+
+
 })
 
 router.get('/register',async (req, res) => {
