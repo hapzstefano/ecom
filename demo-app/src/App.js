826e2e1bff5729a712fe2292cfffb36797f8aed6
hapzstefano <stevanocalvin333@gmail.com>
2021-10-24 21:51:56 +0700

    folder

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -9,18 +9,23 @@ import "./App.css";
 
 //pages
 import Navbar from "./components/Navbar";
+import Home from "./components/Pages/Home";
 import NotFound from "./components/Pages/NotFound";
 import SignIn from "./components/Pages/SignIn";
+import SignUp from "./components/Pages/SignUp";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/login">
           <SignIn />
         </Route>
-        <Route exact path="/buku">
-          <h1>saya ga suka baca buku SE</h1>
+        <Route exact path="/register">
+          <SignUp />
         </Route>
         <Route path="*">
           <Navbar />
