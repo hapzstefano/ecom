830f3107c47d6ebed8086c2ba3193a682853af7d
hapzstefano <stevanocalvin333@gmail.com>
2021-10-25 00:15:23 +0700

    login regis frontend

--- demo-app/src/components/Pages/SignIn/index.js
+++ demo-app/src/components/Pages/SignIn/index.js
@@ -2,46 +2,72 @@ import React, { useState, useEffect } from "react";
 import { EncryptStorage } from "encrypt-storage";
 import $ from "jquery";
 import { useHistory } from "react-router-dom";
+import ButtonRipple from "../../ButtonRipple";
+import "./signin.css";
 const SignIn = () => {
   const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
+  const [password, setPassword] = useState("");
   const encryptStorage = new EncryptStorage(
     `${process.env.REACT_APP_LOCAL_STORAGE_SECRET_KEY}`
   );
   const history = useHistory();
 
-  const handleLogin = (event) => {
+  const handleSignIn = (event) => {
     const formData = new FormData(event.currentTarget);
     event.preventDefault();
-    if (email === "admin" && pass === "admin") {
+    if (email === "admin" && password === "admin") {
       encryptStorage.setItem("admin_logged_in", true);
     } else {
       //DISINI PENGECEKAN EMAIL PASSWORD DR API
       encryptStorage.setItem("user_logged_in", true);
     }
-    history.push("/buku");
+    history.push("/");
   };
   return (
-    <div className="sign-in-box">
-      <form onSubmit={(e) => handleLogin(e)}>
-        <input
-          type="text"
-          name="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="email">Email</label> <br />
-        <input
-          type="password"
-          name="pass"
-          id="pass"
-          value={pass}
-          onChange={(e) => setPass(e.target.value)}
-        />
-        <label htmlFor="pass">Password</label>
-        <button type="submit">Login</button>
-      </form>
+    <div
+      className="container-signup"
+      style={{
+        backgroundImage: "url('/assets/images/global/signup.jpg')",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="content-signup">
+        <h2>Sign In</h2>
+        <form onSubmit={(e) => handleSignIn(e)}>
+          <div className="form-input">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="email">
+              <span>Email</span>{" "}
+            </label>
+          </div>
+          <div className="form-input">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label htmlFor="password">
+              <span>Password</span>{" "}
+            </label>
+          </div>
+
+          <ButtonRipple type="submit" text="Sign Up" />
+        </form>
+        <p className="asterisk">
+          &#42; By logging in, you agree to our Terms of Use and to receive our
+          emails & updates and acknowledge that you read our Privacy Policy.
+        </p>
+      </div>
     </div>
   );
 };
