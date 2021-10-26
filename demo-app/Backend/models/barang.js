2919fed69c88a56ada787db2b410f684789522d9
marvelchbp <57288773+marvelchbp@users.noreply.github.com>
2021-10-26 19:07:00 +0700

    update model Barang.js

--- demo-app/Backend/models/barang.js
+++ demo-app/Backend/models/barang.js
@@ -7,8 +7,29 @@ const barangSchema = new mongoose.Schema({
     stok: {type: Number, required: true},
     gambar: {type: String, required: true},
     status: {type: Number, required: true},
-    id_kategori: {type: String, required: true},
-    id_brand: {type: String, required: true}
+    kategori: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "kategori",
+        required: true
+    },
+    brand: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "brand",
+        required: true
+    },
+    review: [{
+        type: {
+            id_review: {type: String, required: true},
+            comment: {type: String, required: true},
+            rating: {type: String, required: true},
+            author: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "customer",
+                required: true
+            }
+        },
+        required: false    
+    }]
 })
 
 module.exports = mongoose.model('barang', barangSchema)
